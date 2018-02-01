be3bb1de7b9e96fb56704c9524d2e6e9a53b1dae
linfengisep <wang.linfeng0213@gmail.com>
2018-02-02 00:40:57 +0100

    transition drawable,two ways

--- LayoutTraining/app/src/main/java/isep/layouttraining/RoundCornerDrawableActivity.java
+++ LayoutTraining/app/src/main/java/isep/layouttraining/RoundCornerDrawableActivity.java
@@ -1,10 +1,14 @@
 package isep.layouttraining;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 /**
@@ -13,14 +17,57 @@ import android.widget.ImageView;
 
 public class RoundCornerDrawableActivity extends Activity {
     ImageView myLogo;
-
+    ImageButton imageButton;
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_corner_view);
 
+        //shape drawable;
         myLogo = (ImageView)findViewById(R.id.my_image_view);
         myLogo.setImageResource(R.drawable.mlogo);
+
+        //transition drawable;
+        /*
+        imageButton = (ImageButton)findViewById(R.id.transition_btn);
+        TransitionDrawable drawable = (TransitionDrawable)imageButton.getDrawable();
+        drawable.startTransition(500);
+        */
+        //way 2, use java to get a transition effect directly;
+        Drawable background[]=new Drawable[2];
+        background[0] =getResources().getDrawable(R.drawable.on);
+        background[1] =getResources().getDrawable(R.drawable.off);
+
+        TransitionDrawable crossfader= new TransitionDrawable(background);
+
+        ImageButton imagebtn=(ImageButton)findViewById(R.id.transition_btn);
+        imagebtn.setImageDrawable(crossfader);
+        crossfader.startTransition(2000);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
