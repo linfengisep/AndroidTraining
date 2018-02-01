f883e0ae0c9b1e6ea74a64febbe7304621db9df8
linfengisep <wang.linfeng0213@gmail.com>
2018-02-02 00:08:22 +0100

    shape drawable

--- LayoutTraining/app/src/main/java/isep/layouttraining/RoundCornerDrawableActivity.java
+++ LayoutTraining/app/src/main/java/isep/layouttraining/RoundCornerDrawableActivity.java
@@ -5,17 +5,22 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.widget.ImageView;
 
 /**
  * Created by linfengwang on 01/02/2018.
  */
 
 public class RoundCornerDrawableActivity extends Activity {
+    ImageView myLogo;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_corner_view);
+
+        myLogo = (ImageView)findViewById(R.id.my_image_view);
+        myLogo.setImageResource(R.drawable.mlogo);
     }
 }
