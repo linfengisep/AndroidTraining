4bafc84ad9b7f039d60c0bebf425cff62820c077
linfengisep <wang.linfeng0213@gmail.com>
2018-02-17 00:07:51 +0100

    TD1.1

--- ToyApp/app/src/main/java/isep/toy/MainActivity.java
+++ ToyApp/app/src/main/java/isep/toy/MainActivity.java
@@ -2,6 +2,7 @@ package isep.toy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToysListTextView = (TextView) findViewById(R.id.my_toy);
+        //mToysListTextView.setMovementMethod(new ScrollingMovementMethod());
         String toyList []=ToyBox.getToyNames();
-       s=  convertToStringlist(toyList);
-        Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+        s=convertToStringlist(toyList);
         mToysListTextView.setText(s);
     }
 
