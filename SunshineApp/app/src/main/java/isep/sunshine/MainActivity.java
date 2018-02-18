0e784a419aef9eee684a83202de8ef2c7a0197e8
linfengisep <wang.linfeng0213@gmail.com>
2018-02-18 23:47:40 +0100

    menu

--- SunshineApp/app/src/main/java/isep/sunshine/MainActivity.java
+++ SunshineApp/app/src/main/java/isep/sunshine/MainActivity.java
@@ -1,13 +1,18 @@
 package isep.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import java.net.URL;
 
-// TODO (1) Create a field to store the weather display TextView
-// TODO (2) Use findViewById to get a reference to the weather display TextView
-// TODO (3) Create an array of Strings that contain fake weather data
-// TODO (4) Append each String from the fake weather data array to the TextView
+import isep.sunshine.data.SunshinePreferences;
+import isep.sunshine.utilities.NetworkUtils;
+import isep.sunshine.utilities.OpenWeatherJsonUtils;
+
 public class MainActivity extends AppCompatActivity {
     public TextView forecast;
     @Override
@@ -15,9 +20,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
         forecast = (TextView)findViewById(R.id.tv_weather_data);
-        String [] weatherData = new String[]{"Raining","strom","snow","smog"};
-        for(String weather:weatherData){
-            forecast.append(weather+"\n\n");
+        loadWeatherData();
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+     public class FetchWeatherTask extends AsyncTask<String,Void,String[]>{
+        @Override
+        public String[] doInBackground(String... params){
+            if(params.length==0){
+                return null;
+            }
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+            try {
+                String jsonWeatherReponse = NetworkUtils.getResponseFromHttpUrl(
+                        weatherRequestUrl);
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(
+                        MainActivity.this, jsonWeatherReponse);
+                return simpleJsonWeatherData;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
         }
+
+        @Override
+        public void onPostExecute(String[] weatherData){
+            if (weatherData != null) {
+                for (String weatherString : weatherData) {
+                    forecast.append((weatherString) + "\n\n\n");
+                }
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            forecast.setText("");
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
