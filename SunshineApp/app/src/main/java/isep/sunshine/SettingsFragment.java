b297ac869c7a5a062d42c081ddfd202b8dbec890
linfengisep <wang.linfeng0213@gmail.com>
2018-03-04 00:00:21 +0100

    shared preference

--- SunshineApp/app/src/main/java/isep/sunshine/SettingsFragment.java
+++ SunshineApp/app/src/main/java/isep/sunshine/SettingsFragment.java
@@ -2,6 +2,7 @@ package isep.sunshine;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -34,6 +35,17 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_general);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
     }
 
     @Override
