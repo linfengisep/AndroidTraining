acd85c71f3f00bd3d8a9592080f7081049999c5c
linfengisep <wang.linfeng0213@gmail.com>
2018-03-03 23:58:54 +0100

    preference summary

--- SunshineApp/app/src/main/java/isep/sunshine/SettingsFragment.java
+++ SunshineApp/app/src/main/java/isep/sunshine/SettingsFragment.java
@@ -1,8 +1,49 @@
 package isep.sunshine;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
 /**
  * Created by linfengwang on 03/03/2018.
  */
 
-public class SettingsFragment {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+
+    public void setPreferenceSummary(Preference preference,Object value){
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 }
